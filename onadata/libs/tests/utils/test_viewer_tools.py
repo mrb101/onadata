c4c59399a24742e01d8408671c9cb2c7d8f4b2c6
Davis Raymond Muro <davisraymondmuro@outlook.com>
2020-01-31 12:19:32 +0300

    Modify get_form_url function
    Modify the get_form_url function to accept an optional xform_uuid
    of which it will use to generate a url to the form with

--- onadata/libs/tests/utils/test_viewer_tools.py
+++ onadata/libs/tests/utils/test_viewer_tools.py
@@ -155,6 +155,10 @@ class TestViewerTools(TestBase):
         url = get_form_url(request, username='bob', xform_pk=1)
         self.assertEqual(url, 'https://ona.io/bob/1')
 
+        # with form uuid url https://ona.io/e/edrfdfdf
+        url = get_form_url(request, xform_uuid='edrfdfdf')
+        self.assertEqual(url, 'https://ona.io/e/edrfdfdf')
+
     @override_settings(ZIP_REPORT_ATTACHMENT_LIMIT=8)
     @patch('onadata.libs.utils.viewer_tools.report_exception')
     def test_create_attachments_zipfile_file_too_big(self, rpt_mock):
